e715e186e60501696f0ff82c09fe0596f06d1435
Jacques Van Staden <jacques.vs@laborisx.com>
2023-05-31 12:12:19 +0200

    05-31-2023

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <?php
 $html = file_get_contents('https://www.worldometers.info/coronavirus/');
 
@@ -15,46 +16,58 @@ $data = array();
 
 // Iterate through table rows
 foreach ($table->getElementsByTagName('tr') as $row) {
-$rowData = array();
+        $rowData = array();
 
-// Iterate through table cells
-foreach ($row->getElementsByTagName('td') as $cell) {
-$rowData[] = $cell->nodeValue;
-}
+        // Iterate through table cells
+        foreach ($row->getElementsByTagName('td') as $cell) {
+            $rowData[] = $cell->nodeValue;
+        }
 
-$data[] = $rowData;
-}
-
-// Remove the first empty row
-array_shift($data);
-
-// Prepare the table HTML
-$tableHTML = '<table>';
-    $tableHTML .= '<thead>
-                        <tr>
-                            <th>Country</th>
-                            <th>Total Cases</th>
-                            <th>New Cases</th>
-                            <th>Total Deaths</th>
-                            <th>New Deaths</th>
-                            <th>Total Recovered</th>
-                        </tr>
-                    </thead>';
-    $tableHTML .= '<tbody>';
-
-    // Iterate through the data and generate table rows
-    foreach ($data as $row) {
-    $tableHTML .= '<tr>';
-        $tableHTML .= '<td>' . $row[1] . '</td>';
-        $tableHTML .= '<td>' . $row[2] . '</td>';
-        $tableHTML .= '<td>' . $row[3] . '</td>';
-        $tableHTML .= '<td>' . $row[4] . '</td>';
-        $tableHTML .= '<td>' . $row[5] . '</td>';
-        $tableHTML .= '<td>' . $row[6] . '</td>';
-        $tableHTML .= '</tr>';
+        $data[] = $rowData;
     }
 
-    $tableHTML .= '</tbody>';
-    $tableHTML .= '</table>';
+$jsonString = json_encode($data);
 
-echo $tableHTML;
+$decodedData = json_decode($jsonString, true);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 100%;
+            }
+
+            th, td {
+                padding: 8px;
+                text-align: left;
+                border-bottom: 1px solid #ddd;
+            }
+        </style>
+    </head>
+    <body>
+        <table class="table table-striped">
+            <tr>
+                <th>Country</th>
+                <th>Total Cases</th>
+                <th>New Cases</th>
+                <th>Total Deaths</th>
+                <th>New Deaths</th>
+                <th>Total Recovered</th>
+            </tr>
+            <?php foreach ($decodedData as $value){ ?>
+                <tr>
+                    <td><?php echo isset($value['1']) ? $value['1'] : ""; ?></td>
+                    <td><?php echo isset($value['2']) ? $value['2'] : ""; ?></td>
+                    <td><?php echo isset($value['3']) ? $value['3'] : ""; ?></td>
+                    <td><?php echo isset($value['4']) ? $value['4'] : ""; ?></td>
+                    <td><?php echo isset($value['5']) ? $value['5'] : ""; ?></td>
+                    <td><?php echo isset($value['6']) ? $value['6'] : ""; ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </body>
+</html>
